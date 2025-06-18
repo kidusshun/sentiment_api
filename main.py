7921b16cb14df9ce3fb311a0523c337ae361ec23
kidus-hunegnaw <kidusshun@gmail.com>
2025-06-18 12:01:18 +0300

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sentiment_router.router import sentiment_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can restrict this to specific origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(sentiment_router)
 
